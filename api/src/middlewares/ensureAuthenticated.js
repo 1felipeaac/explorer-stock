8e3109187344ac2d9de9d8fe037f84e7cda03e42
Felipe Coelho <felipeaacoelho@gmail.com>
2023-09-18 13:34:52 -0300

    validação de usuario

--- api/src/middlewares/ensureAuthenticated.js
+++ api/src/middlewares/ensureAuthenticated.js
@@ -5,11 +5,11 @@ const authConfig = require('../configs/auth');
 function ensureAuthenticated(request, response, next) {
   const authHeader = request.headers;
 
-  if (!authHeader.coockie) {
+  if (!authHeader.cookie) {
     throw new AppError('JWT token não informado', 401);
   }
 
-  const [, token] = authHeader.split('token=');
+  const [, token] = authHeader.cookie.split('token=');
 
   try {
     const {role,  sub: user_id } = verify(token, authConfig.jwt.secret); // adicionar role na requisição
