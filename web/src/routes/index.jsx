b5518e0c9f900484b7d5c63070180c3a3331469d
Felipe Coelho <felipeaacoelho@gmail.com>
2023-09-18 12:25:48 -0300

    add credentials with cookies and validated User

--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -7,9 +7,11 @@ import { AuthRoutes } from './auth.routes';
 import { CostumerRoutes } from './constumer.routes';
 import { SaleRoutes } from './sale.routes';
 import { USER_ROLE } from '../utils/roles';
+import { useEffect } from 'react';
+import { api } from '../services/api';
 
 export function Routes() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
   function AccessRoute(){
     switch(user.role){
@@ -23,6 +25,16 @@ export function Routes() {
         <CostumerRoutes/>
     }
   }
+
+  useEffect(()=>{
+    api.get('/users/validated').catch((error) => {
+        signOut()
+      // console.log(error)
+      // if(error.reposnse?.status === 401){
+        // signOut()
+      // }
+    })
+  },[])
 
   return (
     <BrowserRouter>
